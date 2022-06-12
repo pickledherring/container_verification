1379d3ac215e0435b0372b488f18c990b8852697
Gabe <gtsalmon@gmail.com>
2022-06-12 15:58:00 -0400

    some evaluation logic, bugfix edits

---
+++ container_rack_truth.js
@@ -0,0 +1,79 @@
+﻿let cont_truth = 
+{
+  "CTR_2mL_Sartstedt_72_694_006_PPD_V1":
+  {
+   "n_segs": 2,
+   "cb_thickness": 1.26,
+   "c_height": 47,
+   "mp_height": 0.15,
+   "b_height": 0,
+   "lsh": 45,
+   "clld": 1,
+   "shape1": "v_cone",
+   "up_ID1": 8.55,
+   "low_ID1": 8.2,
+   "in_SH1": 40.2,
+   "shape2": "v_base",
+   "up_ID2": 8.2,
+   "low_ID2": null,
+   "in_SH2": 4.45
+ },
+ "CTR_2_5mL_Sartstedt_60_614_010_PPD_V1":
+ {
+   "n_segs": 2,
+   "cb_thickness": 0.86,
+   "c_height": 54,
+   "mp_height": 0.5,
+   "b_height": 0,
+   "lsh": 50,
+   "clld": 1,
+   "shape1": "v_cone",
+   "up_ID1": 10.75,
+   "low_ID1": 10.5,
+   "in_SH1": 39.73,
+   "shape2": "v_cone",
+   "up_ID2": 10.5,
+   "low_ID2": 4.3,
+   "in_SH2": 11.76
+ }
+}
+
+let rack_truth =
+{
+  "SMP_CAR_32_2mLSarstedt_A00":{
+    "desc": "Sample carrier with 32 positions with preloaded 2.0mL Sarstedt Tubes ",
+    "type": "reg_rack",
+    "up_width_x": 22.5,
+    "low_width_x": 22.5,
+    "seg_height_x": 140,
+    "n_rows": 32,
+    "plate_height": 140,
+    "plate_ch": 140,
+    "stack_height": 0,
+    "dist_bp_to_ob": 29.3,
+    "dist_bh": 0,
+    "dist_br": 15,
+    "left_oe": 13.96,
+    "front_oe": 14.7,
+    "width": 22.5,
+    "length": 497
+  },
+  "SMP_CAR_32_2_5mL_Sarstedt_60_614_010_PPD_V1.rck":{
+    "rack_desc": "Sample carrier with 32 positions with preloaded 2.0mL Sarstedt Tubes",
+    "type": "reg_rack",
+    "up_width_x": 22.5,
+    "low_width_x": 22.5,
+    "seg_height_x": 140,
+    "n_rows": 32,
+    "plate_height": 140,
+    "plate_ch": 140,
+    "stack_height": 0,
+    "dist_bp_to_ob": 50.5,
+    "dist_bh": 0,
+    "dist_br": 15,
+    "left_oe": 12.34,
+    "front_oe": 14.9,
+    "width": 22.5,
+    "length": 497
+  }
+}
--- logic.js
+++ logic.js
@@ -1,19 +1,78 @@
 // could use sessionStorage to keep the form between switching pages
-// form submission
-let form = document.querySelector("form")
-form.addEventListener("submit", function(event) {
-  let data = new FormData(form)
-  let output = ""
-  for (const entry of data) {
-    output = output + entry[0] + "=" + entry[1] + "\n"
-  }
-  console.log(output)
-  event.preventDefault();
-}, false)
 
+init()
+
+function init() {
+    // populate name drop downs, cont/rack_truth obtained from
+    // container_rack_truth.js
+    for (const name of Object.keys(cont_truth)){
+        let opt_conts = document.getElementById('opt_conts')
+        opt_conts.innerHTML += 
+            `<option>${name}</option>`
+    }
+
+    for (const name of Object.keys(rack_truth)){
+        let opt_racks = document.getElementById('opt_racks')
+        opt_racks.innerHTML += 
+            `<option>${name}</option>`
+    }
+
+    // get form submission
+    let form = document.querySelector("form")
+    form.addEventListener("submit", function(event) {
+    let formData = new FormData(form)
+    let output = {}
+    for (const entry of formData) {
+        output[`${entry[0]}`] = entry[1]
+    }
+
+    // should prevent a page refresh on submission
+    event.preventDefault()
+    // form evaluation logic
+    process(output)
+    }, false)
+}
+
+// to do:
+// 1. add segment field use logic (if n_segs == 1, ignore seg 2)
+// 2. extend to rack
+// 3. send a message with results, tally attempt if failed
+// 4. see if tally is at 3, if so, send another message?
+// 5. add fun elements
+function process(output) {
+    // console.log("output w key:", output["n_segs"])
+    // for (const [key, value] of Object.entries(output)) {
+    //         console.log(key, value)
+    //     }
+
+    // array of fields that need to be exact
+    let exact = ["n_segs", "shape1", "shape2", "type"]
+
+    // container evaluation
+    if ((output.cont_name) && output.cont_name in cont_truth) {
+        for (const [key, trueValue] of Object.entries(cont_truth[output.cont_name])) {
+            if (exact.includes(key) && trueValue == output[key]) {
+                console.log(`exact evalutaion match ${key}: ${trueValue}`)
+            }
+            else if ((trueValue >= output[key] * .95) &&
+                    (trueValue <= output[key] * 1.05)) {
+                console.log(`inexact evaluation match ${key}: ${trueValue}`)
+            }
+            else {
+                console.log(`no match ${key}: ${trueValue}`)
+            }
+        }
+    
+    // rack evaluation
+    }
+    if ((output.rack_name) && output.rack_name in rack_truth) {
+        console.log(rack_truth[output.rack_name])
+    }
+}
+
+// changes the image and measurement options in the "segment approximation"
+// sections based on the radio button clicked
 function changeOps(val, num) {
-    // changes the image and measurement options in the "segment approximation"
-    // sections based on the radio button clicked
     // change image
     let img = document.getElementById(`shape_img${num}`)
     img.src= `images/${val}.png`
